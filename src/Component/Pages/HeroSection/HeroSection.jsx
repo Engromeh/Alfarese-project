d1bb9c3ceaec3e725d74027a4fc34ed015746dec
Mahmoud Saaed Romeh <123378237+Engromeh@users.noreply.github.com>
2025-02-03 20:46:12 +0200

    Creat ifram

--- src/Component/Pages/HeroSection/HeroSection.jsx
+++ src/Component/Pages/HeroSection/HeroSection.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import group1 from "../../../Images/Mask group (1).png";
 import group2 from "../../../Images/Mask group (2).png";
 import group3 from "../../../Images/Mask group.png";
-
+import vide from "../../../Images/00a0ff6a220a5eb3f63a865b31d63c0b.mp4"
 const HeroSection = () => {
+  const [showIframe, setShowIframe] = useState(false);
+
+  const handleClick = () => {
+    setShowIframe(true);
+  };
   return (
     <>
       <section className="hero text-center py-5">
@@ -30,73 +36,88 @@ const HeroSection = () => {
             >
               ابدأ الآن
             </button>
-            <p
-              className="custom-btn fw-bold d-flex align-items-center"
-              style={{ color: "white", fontSize: "20px" }}
+            <div>
+      <p
+        className="custom-btn fw-bold d-flex align-items-center"
+        style={{ color: "white", fontSize: "20px" }}
+      >
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 50 50"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={handleClick}
+        >
+          <path
+            d="M29.8999 23.8193C31.364 24.6654 31.3694 25.7297 29.8999 26.6863L18.546 34.6273C17.1194 35.4168 16.1505 34.9506 16.0488 33.2423L16.0006 16.622C15.9685 15.0484 17.2184 14.6038 18.4095 15.3583L29.8999 23.8193Z"
+            fill="#151723"
+            stroke="url(#paint0_linear_0_1)"
+            strokeWidth="2"
+          />
+          <path
+            d="M33.8999 23.8193C35.364 24.6654 35.3694 25.7297 33.8999 26.6863L22.546 34.6273C21.1194 35.4168 20.1505 34.9506 20.0488 33.2423L20.0006 16.622C19.9685 15.0484 21.2184 14.6038 22.4095 15.3583L33.8999 23.8193Z"
+            fill="#151723"
+            stroke="url(#paint1_linear_0_1)"
+            strokeWidth="2"
+          />
+          <circle
+            cx="25"
+            cy="25"
+            r="24.5"
+            stroke="url(#paint2_linear_0_1)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_0_1"
+              x1="23.5"
+              y1="15"
+              x2="23.5"
+              y2="35"
+              gradientUnits="userSpaceOnUse"
             >
-              <svg
-                width="50"
-                height="50"
-                viewBox="0 0 50 50"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M29.8999 23.8193C31.364 24.6654 31.3694 25.7297 29.8999 26.6863L18.546 34.6273C17.1194 35.4168 16.1505 34.9506 16.0488 33.2423L16.0006 16.622C15.9685 15.0484 17.2184 14.6038 18.4095 15.3583L29.8999 23.8193Z"
-                  fill="#151723"
-                  stroke="url(#paint0_linear_0_1)"
-                  strokeWidth="2"
-                />
-                <path
-                  d="M33.8999 23.8193C35.364 24.6654 35.3694 25.7297 33.8999 26.6863L22.546 34.6273C21.1194 35.4168 20.1505 34.9506 20.0488 33.2423L20.0006 16.622C19.9685 15.0484 21.2184 14.6038 22.4095 15.3583L33.8999 23.8193Z"
-                  fill="#151723"
-                  stroke="url(#paint1_linear_0_1)"
-                  strokeWidth="2"
-                />
-                <circle
-                  cx="25"
-                  cy="25"
-                  r="24.5"
-                  stroke="url(#paint2_linear_0_1)"
-                />
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_0_1"
-                    x1="23.5"
-                    y1="15"
-                    x2="23.5"
-                    y2="35"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#F4CFAB" />
-                    <stop offset="1" stopColor="#D9D9D9" />
-                  </linearGradient>
-                  <linearGradient
-                    id="paint1_linear_0_1"
-                    x1="27.5"
-                    y1="15"
-                    x2="27.5"
-                    y2="35"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#F4CFAB" />
-                    <stop offset="1" stopColor="#D9D9D9" />
-                  </linearGradient>
-                  <linearGradient
-                    id="paint2_linear_0_1"
-                    x1="25"
-                    y1="0"
-                    x2="25"
-                    y2="50"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopColor="#F4CFAB" />
-                    <stop offset="1" stopColor="#D9D9D9" />
-                  </linearGradient>
-                </defs>
-              </svg>
-              فيديو تعريفي
-            </p>
+              <stop stopColor="#F4CFAB" />
+              <stop offset="1" stopColor="#D9D9D9" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear_0_1"
+              x1="27.5"
+              y1="15"
+              x2="27.5"
+              y2="35"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#F4CFAB" />
+              <stop offset="1" stopColor="#D9D9D9" />
+            </linearGradient>
+            <linearGradient
+              id="paint2_linear_0_1"
+              x1="25"
+              y1="0"
+              x2="25"
+              y2="50"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#F4CFAB" />
+              <stop offset="1" stopColor="#D9D9D9" />
+            </linearGradient>
+          </defs>
+        </svg>
+        فيديو تعريفي
+      </p>
+
+      {showIframe && (
+        <div>
+          <iframe
+            width="560"
+            height="315"
+            src={ vide}
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+    </div>
           </div>
 
           <div className="row mt-5">
